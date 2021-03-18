2b3435719ad71219a81706b00631be124d9ce254
blackjuiceXD <claushundeboel@hotmail.com>
2021-03-18 10:54:09 +0100

    Connection to MongoDB

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.get("/api/welcome", (req, res) => {
     res.status(200).send({message: "Welcome to the PWA_Rest_API"});
 })
 
+mongoose.connect(
+    process.env.DBHOST,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+).catch(error => console.log("Error connecting to MongoDB:" + error));
+
+mongoose.connection.once("open", () => console.log("Connected suscessfully  to MongoDB"));
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, function() {

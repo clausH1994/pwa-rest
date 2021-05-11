4134a22159d744cd24ad9e122bd307fd847d2365
blackjuiceXD <claushundeboel@hotmail.com>
2021-05-11 12:54:34 +0200

    role?

--- validation.js
+++ validation.js
@@ -4,8 +4,9 @@ const jwt = require('jsonwebtoken');
 const registerValidation = (data) => {
     const schema = Joi.object(
         {
-            name: Joi.string().min(4).max(255).required(),
+            name: Joi.string().min(2).max(255).required(),
             email: Joi.string().min(6).max(255).required(),
+            role: Joi.string().min(4).max(255).required(),
             password: Joi.string().min(6).max(255).required()
         });
     return schema.validate(data);

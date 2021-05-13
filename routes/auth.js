55d633a5ba6d1c5a5764e5e269f57274f353a47e
clausH1994 <44051013+clausH1994@users.noreply.github.com>
2021-05-13 11:26:48 +0200

    weekHours

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,7 @@ router.post("/register", async (req, res) => {
         name: req.body.name,
         email: req.body.email,
         role: req.body.role,
-        weekhours: req.body.weekhours,
+        weekHours: req.body.weekhours,
         password
     });
 
